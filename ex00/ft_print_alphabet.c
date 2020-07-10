58a57b5038b48955d5ebda93e68c3a0a14456597
Akani Maswanganyi <akmaswan@c5r11s1.wethinkcode.co.za>
2020-07-10 15:21:48 +0200

    exercese 3

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -1,19 +1,7 @@
 #include <unistd.h>
 
-void ft_putcher(char c) {
-	write(1, &c, 1);
-}
-
-void ft_print_alphabet(void) {
-	int count;
-	count=97;
-	while (count<=122)
+void ft_print_alphabet(void)
 	{
-		ft_putcher(count);
-		count++;
+		write(1, "abcdefghijklmnopqrstuvwxyz", 26);
 	}
-}
-int main(void){
-	ft_print_alphabet();
-	return(0);
-}
+
